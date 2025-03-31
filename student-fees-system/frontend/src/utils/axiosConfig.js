da422245bc58e07681ae1a9be8e854a4385865e2
Mern Internship <pawanrathor259@gmail.com>
2025-03-31 09:11:57 +0530

    Add student-fees-system/frontend/src/utils/axiosConfig.js 1743392506621

---
+++ student-fees-system/frontend/src/utils/axiosConfig.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import store from '../store/store';
+
+// Create axios instance
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// Add request interceptor
+api.interceptors.request.use(
+  (config) => {
+    const { token } = store.getState().auth;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Add response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      store.dispatch({ type: 'auth/logout' });
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
